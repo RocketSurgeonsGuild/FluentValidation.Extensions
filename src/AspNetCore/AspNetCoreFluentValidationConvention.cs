5d20656f22f2d48b9b581a91f0244104a9d029bc
David Driscoll <david.driscoll@gmail.com>
2019-11-20 22:12:11 -0500

    fixed issue where result would be 200 status instead of the custom status code

--- src/AspNetCore/AspNetCoreFluentValidationConvention.cs
+++ src/AspNetCore/AspNetCoreFluentValidationConvention.cs
@@ -29,6 +29,41 @@ namespace Rocket.Surgery.AspNetCore.FluentValidation
                 .AddFluentValidation();
             context.Services.AddSingleton<IValidatorInterceptor, ValidatorInterceptor>();
             context.Services.AddSingleton<ProblemDetailsFactory, FluentValidationProblemDetailsFactory>();
+
+            context.Services.Configure<ApiBehaviorOptions>(o =>
+            {
+                ProblemDetailsFactory? _problemDetailsFactory = null;
+                o.InvalidModelStateResponseFactory = context =>
+                {
+                    // ProblemDetailsFactory depends on the ApiBehaviorOptions instance. We intentionally avoid constructor injecting
+                    // it in this options setup to to avoid a DI cycle.
+                    _problemDetailsFactory ??= context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+                    return ProblemDetailsInvalidModelStateResponse(_problemDetailsFactory, context);
+                };
+
+                static IActionResult ProblemDetailsInvalidModelStateResponse(
+                    ProblemDetailsFactory problemDetailsFactory, ActionContext context)
+                {
+                    var problemDetails =
+                        problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState);
+                    ObjectResult result;
+                    if (problemDetails.Status == 400)
+                    {
+                        // For compatibility with 2.x, continue producing BadRequestObjectResult instances if the status code is 400.
+                        result = new BadRequestObjectResult(problemDetails);
+                    }
+                    else
+                    {
+                        result = new ObjectResult(problemDetails) { StatusCode = problemDetails.Status };
+                    }
+
+                    result.ContentTypes.Add("application/problem+json");
+                    result.ContentTypes.Add("application/problem+xml");
+
+                    return result;
+                }
+            });
         }
     }
 }
