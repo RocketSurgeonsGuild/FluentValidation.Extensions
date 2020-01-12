484a82ec9c0ce2198693c24ceeb86f618db7bd00
David Driscoll <david.driscoll@gmail.com>
2020-01-12 13:41:54 -0500

    Added new validator

---
+++ src/FluentValidation/FluentValidationExtensions.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+using JetBrains.Annotations;
+using Rocket.Surgery.Extensions.FluentValidation;
+
+// ReSharper disable once CheckNamespace
+namespace FluentValidation
+{
+    /// <summary>
+    /// Fluent validations
+    /// </summary>
+    [PublicAPI]
+    public static class FluentValidationExtensions
+    {
+        /// <summary>
+        /// Defines a validator on the current rule builder that ensures that the specific value is one of the values given in the list.
+        /// </summary>
+        /// <typeparam name="T">Type of Enum being validated</typeparam>
+        /// <typeparam name="TProperty">Type of property being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="caseSensitive">If the comparison between the string and the enum names should be case sensitive</param>
+        /// <param name="values">The values to match against</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, TProperty> IsOneOf<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, bool caseSensitive, params string[] values)
+            => ruleBuilder.SetValidator(new StringInValidator(values, caseSensitive));
+
+        /// <summary>
+        /// Defines a validator on the current rule builder that ensures that the specific value is one of the values given in the list.
+        /// </summary>
+        /// <typeparam name="T">Type of Enum being validated</typeparam>
+        /// <typeparam name="TProperty">Type of property being validated</typeparam>
+        /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+        /// <param name="values">The values to match against</param>
+        /// <returns></returns>
+        public static IRuleBuilderOptions<T, TProperty> IsOneOf<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, params string[] values)
+            => ruleBuilder.SetValidator(new StringInValidator(values, false));
+    }
+}
